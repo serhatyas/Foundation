642b63b6e596c69d00d2b919458f9dc26ea95529
Serhat Yaş <serhatyas15@gmail.com>
2025-02-10 02:03:34 +0300

    Proje dosyası ekle.

---
+++ Services/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Services
+{
+    public class Class1
+    {
+        public class Class2 
+        {
+            //toplama işlemi yapan bi fonksiyon yaz
+            // Bu fonksiyon iki sayıyı toplar
+            int a = 1;
+
+        }
+
+
+
+
+
+
+    }
+}
